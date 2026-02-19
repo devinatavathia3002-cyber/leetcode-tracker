a015e1b9e81dcb360705aaae6b795dec1f61c36f
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-02-18 23:13:47 -0800

    File updated at 2/18/2026, 11:13:47 PM

--- 752-OpentheLock/752-OpentheLock.py
+++ 752-OpentheLock/752-OpentheLock.py
@@ -1,39 +1,37 @@
-# Last updated: 2/18/2026, 11:02:56 PM
+# Last updated: 2/18/2026, 11:13:47 PM
 1class Solution:
 2    def openLock(self, deadends: List[str], target: str) -> int:
 3        
 4        # let's try this with standard bfs first
 5        visited = set(deadends)
 6        q = deque()
-7        q.append("0000")
-8        steps = 0
-9
-10        # edge case
-11        if target in deadends or "0000" in deadends:
-12            return -1
-13
-14        # main loop
-15        while q:
-16            steps += 1
-17            for j in range(len(q)):
-18                val = q.popleft()
-19                if val == target:
-20                    return steps - 1
-21
-22                for i in range(4):
-23                    digit1 = (int(val[i]) + 1) % 10
-24                    val1 = val[:i] + str(digit1) + val[i + 1:]
-25
-26                    digit2 = (int(val[i]) - 1 + 10) % 10
-27                    val2 = val[:i] + str(digit2) + val[i + 1:]
-28
-29                    if val1 == target or val2 == target:
-30                        return steps
-31                    if val1 not in visited:
-32                        q.append(val1)
-33                        visited.add(val1)
-34                    if val2 not in visited:
-35                        q.append(val2)
-36                        visited.add(val2)
-37
-38        return -1
+7        q.append(("0000", 0))
+8
+9        # edge case
+10        if target in deadends or "0000" in deadends:
+11            return -1
+12
+13        # main loop
+14        while q:
+15            for j in range(len(q)):
+16                val, steps = q.popleft()
+17                if val == target:
+18                    return steps
+19
+20                for i in range(4):
+21                    digit1 = (int(val[i]) + 1) % 10
+22                    val1 = val[:i] + str(digit1) + val[i + 1:]
+23
+24                    digit2 = (int(val[i]) - 1 + 10) % 10
+25                    val2 = val[:i] + str(digit2) + val[i + 1:]
+26
+27                    if val1 == target or val2 == target:
+28                        return steps + 1
+29                    if val1 not in visited:
+30                        q.append((val1, steps + 1))
+31                        visited.add(val1)
+32                    if val2 not in visited:
+33                        q.append((val2, steps + 1))
+34                        visited.add(val2)
+35
+36        return -1
