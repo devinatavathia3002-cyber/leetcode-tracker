9ae7f0d384cd25127e36fcf31fbdc454892ddfe8
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-02-19 20:30:56 -0800

    File updated at 2/19/2026, 8:30:56 PM

--- 752-OpentheLock/752-OpentheLock.py
+++ 752-OpentheLock/752-OpentheLock.py
@@ -1,37 +1,44 @@
-# Last updated: 2/18/2026, 11:13:47 PM
+# Last updated: 2/19/2026, 8:30:56 PM
 1class Solution:
 2    def openLock(self, deadends: List[str], target: str) -> int:
 3        
-4        # let's try this with standard bfs first
-5        visited = set(deadends)
-6        q = deque()
-7        q.append(("0000", 0))
+4        visited = set(deadends)
+5
+6        beg = {"0000"}
+7        end = {target}
 8
-9        # edge case
-10        if target in deadends or "0000" in deadends:
-11            return -1
-12
-13        # main loop
-14        while q:
-15            for j in range(len(q)):
-16                val, steps = q.popleft()
-17                if val == target:
-18                    return steps
-19
-20                for i in range(4):
-21                    digit1 = (int(val[i]) + 1) % 10
-22                    val1 = val[:i] + str(digit1) + val[i + 1:]
+9        steps = 0
+10
+11        if target in visited or "0000" in visited:
+12            return -1
+13        
+14        if target == "0000":
+15            return 0
+16
+17        while beg and end:
+18            if len(beg) > len(end):
+19                beg, end = end, beg
+20            
+21            temp = set()
+22            steps += 1
 23
-24                    digit2 = (int(val[i]) - 1 + 10) % 10
-25                    val2 = val[:i] + str(digit2) + val[i + 1:]
-26
-27                    if val1 == target or val2 == target:
-28                        return steps + 1
-29                    if val1 not in visited:
-30                        q.append((val1, steps + 1))
-31                        visited.add(val1)
-32                    if val2 not in visited:
-33                        q.append((val2, steps + 1))
-34                        visited.add(val2)
-35
-36        return -1
+24            for lock in beg:
+25                
+26                for j in range(4):
+27                    newVal1 = lock[:j] + str((int(lock[j]) + 1) % 10) + lock[j + 1:]
+28                    newVal2 = lock[:j] + str((int(lock[j]) - 1 + 10) % 10) + lock[j + 1:]
+29
+30                    if newVal1 in end or newVal2 in end:
+31                        return steps
+32                    
+33                    if newVal1 not in visited:
+34                        visited.add(newVal1)
+35                        temp.add(newVal1)
+36                    
+37                    if newVal2 not in visited:
+38                        visited.add(newVal2)
+39                        temp.add(newVal2)
+40
+41            beg = temp
+42
+43        return -1
