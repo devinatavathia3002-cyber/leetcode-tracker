6f057f4fdf6637b26e939b41288191fcbb29b0a2
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-02-26 22:04:26 -0800

    File updated at 2/26/2026, 10:04:26 PM

--- 225-ImplementStackusingQueues/225-ImplementStackusingQueues.py
+++ 225-ImplementStackusingQueues/225-ImplementStackusingQueues.py
@@ -1,21 +1,20 @@
-# Last updated: 2/26/2026, 10:02:21 PM
+# Last updated: 2/26/2026, 10:04:26 PM
 1class MyStack:
 2
 3    def __init__(self):
 4        self.q1 = deque()
-5        self.q2 = deque()
-6
-7    def push(self, x: int) -> None:
-8        self.q2.append(x)
-9        while self.q1:
-10            self.q2.append(self.q1.popleft())
-11        self.q1, self.q2 = self.q2, self.q1
-12
-13    def pop(self) -> int:
-14        return self.q1.popleft()
-15
-16    def top(self) -> int:
-17        return self.q1[0]
-18
-19    def empty(self) -> bool:
-20        return len(self.q1) == 0
+5
+6    def push(self, x: int) -> None:
+7        old = len(self.q1)
+8        self.q1.append(x)
+9        for i in range(old):
+10            self.q1.append(self.q1.popleft())
+11
+12    def pop(self) -> int:
+13        return self.q1.popleft()
+14
+15    def top(self) -> int:
+16        return self.q1[0]
+17
+18    def empty(self) -> bool:
+19        return len(self.q1) == 0
