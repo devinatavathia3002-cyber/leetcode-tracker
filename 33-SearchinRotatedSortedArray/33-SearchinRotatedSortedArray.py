0b3afbc4d72f76b56abe66f09fa17d1eff873e39
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-03-05 22:54:19 -0800

    File updated at 3/5/2026, 10:54:19 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,30 +1,30 @@
-# Last updated: 3/1/2026, 7:25:12 PM
+# Last updated: 3/5/2026, 10:54:19 PM
 1class Solution:
-2    def search(self, nums: List[int], target: int) -> int:
-3        l, r = 0, len(nums) - 1
-4
-5        while l < r:
-6            m = ((r - l) // 2) + l
-7            if nums[m] > nums[r]:
-8                l = m + 1
-9            else:
-10                r = m
-11
-12        pivot = r
-13        l, r = 0, len(nums) - 1
-14
-15        if target >= nums[pivot] and target <= nums[r]:
-16            l = pivot
-17        else:
-18            r = pivot - 1
-19
-20        while l <= r:
-21            m = ((r - l) // 2) + l
-22            if nums[m] == target:
-23                return m
-24            elif nums[m] < target:
-25                l = m + 1
-26            else:
-27                r = m - 1
+2    def search(self, nums: List[int], target: int) -> bool:
+3        
+4        l, r = 0, len(nums) - 1
+5        
+6        while l <= r:
+7
+8            m = ((r - l)//2) + l
+9            print(m)
+10
+11            if nums[m] == target:
+12                return m
+13
+14            elif nums[l] < nums[m]:
+15                if nums[l] <= target < nums[m]:
+16                    r = m - 1
+17                else:
+18                    l = m + 1
+19            elif nums[l] > nums[m]:
+20                if nums[m] < target <= nums[r]:
+21                    l = m + 1
+22                else:
+23                    r = m - 1
+24            else:
+25                l += 1
+26
+27        return -1
 28
-29        return -1
+29
