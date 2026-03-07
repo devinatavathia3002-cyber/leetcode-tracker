37aa3d24dad84e8658153c8c1a39a180b00d84d0
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-03-07 13:03:35 -0800

    File updated at 3/7/2026, 1:03:35 PM

--- 658-FindKClosestElements/658-FindKClosestElements.py
+++ 658-FindKClosestElements/658-FindKClosestElements.py
@@ -1,28 +1,14 @@
-# Last updated: 3/5/2026, 8:31:28 PM
+# Last updated: 3/7/2026, 1:03:35 PM
 1class Solution:
 2    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-3        
-4        l = 0
-5        r = 0
-6
-7        output = [0] * k
-8        difference = float("infinity")
-9
-10        testing = 0
-11
-12        while r < len(arr):
-13
-14            if (r - l + 1) > k:
-15                testing -= abs(x - arr[l])
-16                l += 1
-17            
-18            testing += abs(x - arr[r])
-19
-20            if (r - l + 1) == k:
-21                if testing < difference:
-22                    difference = testing
-23                    output = arr[l:r+1]
-24
-25            r += 1
-26        
-27        return output
+3        l, r = 0, len(arr) - k
+4        
+5        while l < r:
+6            m = ((r - l) // 2) + l # right-biased midpoint
+7            
+8            if x - arr[m] <= arr[m + k] - x:
+9                r = m
+10            else:
+11                l = m + 1
+12                
+13        return arr[l:l+k]
