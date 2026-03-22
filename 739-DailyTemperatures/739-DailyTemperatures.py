d924d232b8f955b33cd49f2122e603724ae7ea54
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-03-22 16:55:59 -0700

    File updated at 3/22/2026, 4:55:59 PM

--- 739-DailyTemperatures/739-DailyTemperatures.py
+++ 739-DailyTemperatures/739-DailyTemperatures.py
@@ -1,4 +1,4 @@
-# Last updated: 2/19/2026, 9:36:17 PM
+# Last updated: 3/22/2026, 4:55:59 PM
 1class Solution:
 2    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
 3        
@@ -6,20 +6,15 @@
 5        s = []
 6
 7        for i in range(len(temperatures) - 1, -1, -1):
-8            if len(s) == 0:
-9                s.append([temperatures[i], i])
-10                temperatures[i] = 0
-11            
-12            else:
-13                while s and s[-1][0] <= temperatures[i]:
-14                    s.pop()
-15                
-16                if len(s) == 0:
-17                    s.append([temperatures[i], i])
-18                    temperatures[i] = 0
-19                else:
-20                    warmer, index = s[-1]
-21                    s.append([temperatures[i], i])
-22                    temperatures[i] = (index - i)
-23        
-24        return temperatures
+8            while s and s[-1][0] <= temperatures[i]:
+9                s.pop()
+10                
+11            if len(s) == 0:
+12                s.append([temperatures[i], i])
+13                temperatures[i] = 0
+14            else:
+15                warmer, index = s[-1]
+16                s.append([temperatures[i], i])
+17                temperatures[i] = (index - i)
+18        
+19        return temperatures
