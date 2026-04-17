10fcc31a4333e547a4a58311d89f5150a8e41def
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-04-17 00:46:23 -0700

    Create 207-CourseSchedule

---
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -0,0 +1,30 @@
+# Last updated: 4/17/2026, 12:46:23 AM
+1class Solution:
+2    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+3        
+4        pre = defaultdict(list)
+5        visited = set()
+6        for pair in prerequisites:
+7            past, post = pair
+8            pre[past].append(post)
+9        
+10        def dfs(num):
+11            nonlocal visited
+12            if pre[num] == []:
+13                return True
+14            if num in visited:
+15                return False
+16            
+17            visited.add(num)
+18            for prereq in pre[num]:
+19                if not dfs(prereq):
+20                    return False
+21            
+22            visited.remove(num)
+23            pre[num] = []
+24            return True
+25            
+26        for num in range(numCourses):
+27            if not dfs(num):
+28                return False
+29        return True
