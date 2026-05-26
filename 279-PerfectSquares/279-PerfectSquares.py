d6b9e899b0cf2e93e89da936935fe6d1cfdcbc71
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-05-25 19:27:27 -0700

    File updated at 5/25/2026, 7:27:27 PM

--- 279-PerfectSquares/279-PerfectSquares.py
+++ 279-PerfectSquares/279-PerfectSquares.py
@@ -1,39 +1,32 @@
-# Last updated: 5/23/2026, 9:34:05 PM
+# Last updated: 5/25/2026, 7:27:27 PM
 1class Solution:
 2    def numSquares(self, n: int) -> int:
-3        # with cache
-4        # dp = {} # dp cache
-5    
-6        # def recurse(total):
-7        #     if total == n:
-8        #         return 0
-9        #     if total > n:
-10        #         return float("inf")
-11        #     if total in dp:
-12        #         return dp[total]
-13            
-14        #     res = n
-15        #     for i in range(1, int(n ** 0.5) + 1):
-16        #         new = i * i
-17        #         res = min(res, recurse(total + new) + 1)
-18        #     dp[total] = res
-19        #     return res
-20        
-21        # return recurse(0)
-22
-23        # with dp
-24
-25        dp = [n] * (n + 1)
-26        dp[0] = 0
-27
-28        for i in range(1, n + 1):
-29            for j in range(1, i + 1):
-30                new = j * j
-31                if new > i:
-32                    break
-33                else:
-34                    dp[i] = min(dp[i], 1 + dp[i - new])
-35        
-36        return dp[n]
-37
-38            
+3        
+4        # recursion
+5        # def recurse(total):
+6        #     if total == n:
+7        #         return 0
+8        #     if total > n:
+9        #         return float("inf")
+10            
+11        #     res = n
+12        #     for num in range(1, n):
+13        #         val = num * num
+14        #         res = min(res, recurse(total + val) + 1)
+15        #     return res
+16
+17        # return recurse(0)
+18
+19        # dp
+20        dp = [n] * (n + 1)
+21        dp[0] = 0
+22        dp[1] = 1
+23
+24        for index in range(2, n + 1):
+25            for num in range(1, int(index ** 0.5) + 1):
+26                val = num * num
+27                if val > index:
+28                    continue
+29                dp[index] = min(dp[index], dp[index - val] + 1)
+30        
+31        return dp[n]
