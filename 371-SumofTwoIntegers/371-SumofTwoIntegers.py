8b65e1b912be8a7cef00126d0fd3b55759c13904
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-05-29 18:54:14 -0700

    File updated at 5/29/2026, 6:54:14 PM

--- 371-SumofTwoIntegers/371-SumofTwoIntegers.py
+++ 371-SumofTwoIntegers/371-SumofTwoIntegers.py
@@ -1,4 +1,14 @@
-# Last updated: 5/29/2026, 12:33:56 AM
+# Last updated: 5/29/2026, 6:54:14 PM
 1class Solution:
 2    def getSum(self, a: int, b: int) -> int:
-3        return a + b
+3        mask = 0xffffffff
+4
+5        while b != 0:
+6            tmp = (a & b) << 1
+7            a = (a ^ b) & mask
+8            b = tmp & mask
+9
+10        if a > mask // 2:
+11            return ~(a ^ mask)
+12        else:
+13            return a
