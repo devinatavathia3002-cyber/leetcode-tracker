09ab3b80f17e2f12be181025e76e717e95c118ee
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-05-31 15:25:41 -0700

    File updated at 5/31/2026, 3:25:41 PM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,22 +1,33 @@
-# Last updated: 5/31/2026, 2:57:16 PM
+# Last updated: 5/31/2026, 3:25:41 PM
 1class Solution:
 2    def plusOne(self, digits: List[int]) -> List[int]:
 3        
-4        digits.reverse()
-5        res = []
-6
-7        carry = 1
-8
-9        for digit in digits:
-10            carry += digit
-11            if carry >= 10:
-12                carry = 1
-13                res.append(0)
-14            else:
-15                res.append(carry)
-16                carry = 0
-17                
-18        if carry == 1:
-19            res.append(carry)
-20        res.reverse()
-21        return res
+4        # # iteration 1 solution
+5        # digits.reverse()
+6        # res = []
+7
+8        # carry = 1
+9
+10        # for digit in digits:
+11        #     carry += digit
+12        #     if carry >= 10:
+13        #         carry = 1
+14        #         res.append(0)
+15        #     else:
+16        #         res.append(carry)
+17        #         carry = 0
+18
+19        # if carry == 1:
+20        #     res.append(carry)
+21        # res.reverse()
+22        # return res
+23
+24        # iteraiton 2
+25        for i in range(len(digits) - 1, - 1, -1):
+26            if digits[i] + 1 <= 9:
+27                digits[i] += 1
+28                return digits
+29            else:
+30                digits[i] = 0
+31        
+32        return [1] + digits
