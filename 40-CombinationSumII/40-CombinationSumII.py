eff4a50373e982df5ee5998ab50cc7ff3bab1ed8
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-06-10 21:05:22 -0700

    File updated at 6/10/2026, 9:05:22 PM

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,26 +1,26 @@
-# Last updated: 4/8/2026, 12:37:59 AM
+# Last updated: 6/10/2026, 9:05:22 PM
 1class Solution:
 2    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
 3        
-4        res = []
-5        candidates.sort()
-6
-7        def backtracking(index, subset, total):
-8            nonlocal res
-9
-10            if total == target:
-11                res.append(subset.copy())
-12                return
-13
-14            if total > target:
-15                return
-16
-17            for i in range(index, len(candidates)):
-18                if i > index and candidates[i] == candidates[i - 1]:
-19                    continue
-20                subset.append(candidates[i])
-21                backtracking(i + 1, subset, total + candidates[i])
-22                subset.pop()
+4        # subsets with target sum
+5        output = []
+6        candidates.sort()
+7
+8        def subsets(i, total, sub):
+9            if total == target:
+10                output.append(sub.copy())
+11                return
+12            
+13            if total > target:
+14                return
+15            
+16            for j in range(i, len(candidates)):
+17                if j > 0 and candidates[j] == candidates[j - 1] and j != i:
+18                    continue
+19                sub.append(candidates[j])
+20                subsets(j + 1, total + candidates[j], sub)
+21                sub.pop()
+22
 23
-24        backtracking(0, [], 0)
-25        return res
+24        subsets(0, 0, [])
+25        return output
