ea69b6aadfcdb69d1900e75107f950eb8961b498
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-06-12 16:43:13 -0700

    File updated at 6/12/2026, 4:43:13 PM

--- 743-NetworkDelayTime/743-NetworkDelayTime.py
+++ 743-NetworkDelayTime/743-NetworkDelayTime.py
@@ -1,27 +1,35 @@
-# Last updated: 4/21/2026, 12:29:52 AM
+# Last updated: 6/12/2026, 4:43:13 PM
 1class Solution:
 2    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
 3        
 4        visited = set()
-5        total = 0
-6        minHeap = [[0, k]] # sum, value
-7        adj = defaultdict(list) # [time, endNode]
+5        path = defaultdict(list)
+6
+7        # from/to weight mapping
 8        for time in times:
-9            src, dest, weight = time
-10            adj[src].append([weight, dest])
-11
-12        while minHeap and len(visited) < n:
-13            cumulative, val = heapq.heappop(minHeap)
-14            if val in visited:
-15                continue
-16            visited.add(val)
-17            total = cumulative
-18
-19            for node in adj[val]:
-20                time, value = node
-21                heapq.heappush(minHeap, [cumulative + time, value])
-22
-23        print(total)
-24        return total if len(visited) == n else -1
-25
-26
+9            u, v, w = time
+10            path[u].append((v, w))
+11        
+12        minHeap = []
+13        t = 0
+14
+15        heapq.heappush(minHeap, (0, k))
+16
+17        while len(minHeap) > 0 and len(visited) < n:
+18            weight, curr = heapq.heappop(minHeap)
+19            if curr in visited:
+20                continue
+21            visited.add(curr)
+22            t = weight # cumulative weight
+23
+24            for outgoing in path[curr]:
+25                end, w = outgoing
+26                if end not in visited:
+27                    heapq.heappush(minHeap, (weight + w, end))
+28                else:
+29                    continue
+30
+31        return t if len(visited) == n else -1
+32        
+33
+34
