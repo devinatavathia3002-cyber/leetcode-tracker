b9aa28aa3394ca2ec2cb9382e38e68aeabd17518
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-06-14 00:43:00 -0700

    Create 306-AdditiveNumber

---
+++ 306-AdditiveNumber/306-AdditiveNumber.py
@@ -0,0 +1,37 @@
+# Last updated: 6/14/2026, 12:43:00 AM
+1class Solution:
+2    def isAdditiveNumber(self, num: str) -> bool:
+3        
+4        if len(num) < 3:
+5            return False
+6
+7        def additive(first, second, remaining):
+8            if len(remaining) == 0:
+9                return True
+10            
+11            total = first + second
+12            if remaining.startswith(str(total)):
+13                first = second
+14                second = total
+15                if additive(first, second, remaining[len(str(total)):]):
+16                    return True
+17
+18            return False
+19
+20        # brute force starting two numbers
+21        for s in range(1, len(num)):
+22            for e in range(s, len(num) - 1):
+23                first = num[:s]
+24                second = num[s:e + 1]
+25
+26                if len(first) > 1 and int(first[0]) == 0:
+27                    continue
+28                if len(second) > 1 and int(second[0]) == 0:
+29                    continue
+30                
+31                print(second)
+32 
+33                if additive(int(first), int(second), num[(len(first) + len(second)):]):
+34                    return True
+35        
+36        return False
