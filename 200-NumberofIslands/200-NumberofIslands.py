c8489b6bd09d0a6d88f7d0dca6873dfe98f22048
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-06-14 17:11:53 -0700

    File updated at 6/14/2026, 5:11:53 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,33 +1,27 @@
-# Last updated: 4/14/2026, 8:47:49 PM
+# Last updated: 6/14/2026, 5:11:53 PM
 1class Solution:
 2    def numIslands(self, grid: List[List[str]]) -> int:
-3        rows = len(grid)
-4        cols = len(grid[0])
-5        output = 0
-6
-7        def dfs(r, c):
-8            if r < 0 or c < 0 or r >= rows or c >= cols:
-9                return
-10            
-11            if grid[r][c] == "1":
-12                grid[r][c] = "."
-13
-14                dfs(r + 1, c)
-15                dfs(r - 1, c)
-16                dfs(r, c + 1)
-17                dfs(r, c - 1)
-18            
-19            else:
-20                return
-21
-22        for r in range(rows):
-23            for c in range(cols):
-24                if grid[r][c] == "1":
-25                    dfs(r, c)
-26                    output += 1
-27        
-28        return output
-29
-30
-31
-32        
+3        
+4        # dfs traversal 
+5        ROWS = len(grid)
+6        COLS = len(grid[0])
+7
+8        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+9        output = 0
+10
+11        def dfs(r, c):
+12            if 0 <= r < ROWS and 0 <= c < COLS and grid[r][c] == "1":
+13                grid[r][c] = "0"
+14                for cord in directions:
+15                    x, y = cord
+16                    newR = x + r
+17                    newC = y + c
+18                    dfs(newR, newC)
+19
+20        for r in range(ROWS):
+21            for c in range(COLS):
+22                if grid[r][c] == "1":
+23                    dfs(r, c)
+24                    output += 1
+25
+26        return output
