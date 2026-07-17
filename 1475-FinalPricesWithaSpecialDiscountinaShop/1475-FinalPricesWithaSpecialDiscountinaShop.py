bc388428fdedd46f934f81597c2c7ea5fd1828f7
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-07-16 17:07:56 -0700

    File updated at 7/16/2026, 5:07:56 PM

--- 1475-FinalPricesWithaSpecialDiscountinaShop/1475-FinalPricesWithaSpecialDiscountinaShop.py
+++ 1475-FinalPricesWithaSpecialDiscountinaShop/1475-FinalPricesWithaSpecialDiscountinaShop.py
@@ -1,18 +1,15 @@
-# Last updated: 7/16/2026, 4:58:17 PM
+# Last updated: 7/16/2026, 5:07:56 PM
 1class Solution:
 2    def finalPrices(self, prices: List[int]) -> List[int]:
 3        stack = []
 4
 5        for i in range(len(prices) - 1, - 1, -1):
 6            curr = prices[i]
-7            if len(stack) == 0:
-8                stack.append(curr)
-9            else:
-10                while stack and stack[-1] > curr:
-11                    stack.pop()
-12                if stack and stack[-1] <= curr:
-13                    prices[i] -= stack[-1]
-14            stack.append(curr)
-15        
-16        return prices
-17            
+7            while stack and stack[-1] > curr:
+8                stack.pop()
+9            if stack and stack[-1] <= curr:
+10                prices[i] -= stack[-1]
+11            stack.append(curr)
+12        
+13        return prices
+14            
