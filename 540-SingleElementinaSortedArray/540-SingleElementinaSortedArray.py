6031d661823a65030c80465490358216ad220fee
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-07-23 09:42:38 -0700

    File updated at 7/23/2026, 9:42:38 AM

--- 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.py
+++ 540-SingleElementinaSortedArray/540-SingleElementinaSortedArray.py
@@ -1,4 +1,4 @@
-# Last updated: 7/23/2026, 9:39:40 AM
+# Last updated: 7/23/2026, 9:42:38 AM
 1class Solution:
 2    def singleNonDuplicate(self, nums: List[int]) -> int:
 3        # binary search
@@ -10,14 +10,10 @@
 9            else:
 10                if mid > 0 and nums[mid - 1] == nums[mid]:
 11                    left = (mid - 1)
-12                    if left % 2:
-13                        r = mid - 1
-14                    else:
-15                        l = mid + 1
+12                else:
+13                    left = (mid)
+14                if left % 2:
+15                    r = mid - 1
 16                else:
-17                    left = (mid)
-18                    if left % 2:
-19                        r = mid - 1
-20                    else:
-21                        l = mid + 1
-22        return -1
+17                    l = mid + 1
+18        return -1
