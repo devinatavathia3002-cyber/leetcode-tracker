a4845bb9f74b0f6b155e342af9da2787831c46b5
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-07-27 18:51:10 -0700

    File updated at 7/27/2026, 6:51:10 PM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,16 +1,20 @@
-# Last updated: 7/27/2026, 4:56:27 PM
+# Last updated: 7/27/2026, 6:51:10 PM
 1class Solution:
 2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 3
 4        # using a character map
-5
-6        d = defaultdict(list)
-7        for i in range(len(strs)):
-8            chars = [0] * 26
-9            for s in strs[i]:
-10                chars[ord(s) - ord('a')] += 1
-11            d[tuple(chars)].append(strs[i])
-12        
-13        return list(d.values())
-14            
-15        
+5        mapping = defaultdict(list)
+6
+7        for string in strs:
+8            charMap = [0] * 26
+9            for char in string:
+10                index = ord('a') - ord(char)
+11                charMap[index] += 1
+12            mapping[tuple(charMap)].append(string)
+13        
+14        output = []
+15        for key, val in mapping.items():
+16            output.append(val)
+17        return output
+18            
+19        
