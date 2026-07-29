31895fff400bc0fa168315e0bcb436450b78adf3
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-07-28 22:14:04 -0700

    File updated at 7/28/2026, 10:14:04 PM

--- 210-CourseScheduleII/210-CourseScheduleII.py
+++ 210-CourseScheduleII/210-CourseScheduleII.py
@@ -1,27 +1,27 @@
-# Last updated: 6/13/2026, 8:04:25 PM
+# Last updated: 7/28/2026, 10:14:04 PM
 1class Solution:
 2    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-3        
-4        output = []
-5        adj = defaultdict(list)
-6        count = defaultdict(int)
-7        for course in prerequisites:
-8            end, beg = course
-9            adj[end].append(beg)
-10            count[beg] += 1
-11
-12        q = deque()
-13        for i in range(numCourses):
-14            if i not in count:
-15                q.append(i)
-16
+3        count = defaultdict(int)
+4        adj = defaultdict(list)
+5
+6        for prereq in prerequisites:
+7            crs, before = prereq
+8            adj[crs].append(before)
+9            count[before] += 1
+10
+11        q = deque()
+12        for i in range(numCourses):
+13            if i not in count:
+14                q.append(i)
+15        
+16        output = []
 17        while q:
-18            course = q.popleft()
-19            output.append(course)
-20
-21            for prereq in adj[course]:
-22                count[prereq] -= 1
-23                if count[prereq] <= 0:
-24                    q.append(prereq)
-25
-26        return output[::-1] if len(output) == numCourses else []
+18            popped = q.pop()
+19            output.append(popped)
+20            for crs in adj[popped]:
+21                count[crs] -= 1
+22                if count[crs] == 0:
+23                    q.append(crs)
+24
+25        return output[::-1] if len(output) == numCourses else []
+26
