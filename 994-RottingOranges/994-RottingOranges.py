d19bf1fe9d6e72ced4f7c97cbae09597b04bc458
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-07-29 18:30:55 -0700

    File updated at 7/29/2026, 6:30:55 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,4 +1,4 @@
-# Last updated: 7/29/2026, 6:26:36 PM
+# Last updated: 7/29/2026, 6:30:55 PM
 1class Solution:
 2    def orangesRotting(self, grid: List[List[int]]) -> int:
 3        q = deque()
@@ -12,24 +12,23 @@
 11                if grid[r][c] == 2:
 12                    q.append((r, c))
 13        
-14
-15        mins = 0
-16        while q:
-17            mins += 1
-18            length = len(q)
-19            for i in range(length):
-20                row, col = q.popleft()
-21                for dir in directions:
-22                    x, y = dir
-23                    newR = x + row
-24                    newC = y + col
-25                    if newR >= 0 and newR < ROWS and newC >= 0 and newC < COLS and grid[newR][newC] == 1:
-26                        q.append((newR, newC))
-27                        grid[newR][newC] = 2
-28
-29        for r in range(ROWS):
-30            for c in range(COLS):
-31                if grid[r][c] == 1:
-32                    return -1
-33        
-34        return mins - 1 if mins > 0 else 0
+14        mins = 0
+15        while q:
+16            mins += 1
+17            length = len(q)
+18            for i in range(length):
+19                row, col = q.popleft()
+20                for dir in directions:
+21                    x, y = dir
+22                    newR = x + row
+23                    newC = y + col
+24                    if newR >= 0 and newR < ROWS and newC >= 0 and newC < COLS and grid[newR][newC] == 1:
+25                        q.append((newR, newC))
+26                        grid[newR][newC] = 2
+27
+28        for r in range(ROWS):
+29            for c in range(COLS):
+30                if grid[r][c] == 1:
+31                    return -1
+32        
+33        return mins - 1 if mins > 0 else 0
