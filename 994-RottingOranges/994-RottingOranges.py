1fcdaa726ed0543c720f6757f8ad3ed08b2397ba
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-07-29 18:26:36 -0700

    File updated at 7/29/2026, 6:26:36 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,44 +1,35 @@
-# Last updated: 4/14/2026, 11:33:50 PM
+# Last updated: 7/29/2026, 6:26:36 PM
 1class Solution:
 2    def orangesRotting(self, grid: List[List[int]]) -> int:
-3        rows = len(grid)
-4        cols = len(grid[0])
-5        output = 0
-6        fresh = 0
-7        q = deque()
+3        q = deque()
+4        directions = [(0, 1), (1,0), (-1, 0), (0, -1)]
+5
+6        ROWS = len(grid)
+7        COLS = len(grid[0])
 8
-9        for r in range(rows):
-10            for c in range(cols):
+9        for r in range(ROWS):
+10            for c in range(COLS):
 11                if grid[r][c] == 2:
 12                    q.append((r, c))
-13                if grid[r][c] == 1:
-14                    fresh += 1
-15                    
-16        while q and fresh > 0:
-17            for val in range(len(q)):
-18                row, col = q.popleft()
-19                if row > 0:
-20                    if grid[row - 1][col] == 1:
-21                        fresh -= 1
-22                        grid[row - 1][col] = 2
-23                        q.append((row - 1, col))
-24                if row < rows - 1:
-25                    if grid[row + 1][col] == 1:
-26                        fresh -= 1
-27                        grid[row + 1][col] = 2
-28                        q.append((row + 1, col))
-29                if col > 0:
-30                    if grid[row][col - 1] == 1:
-31                        fresh -= 1
-32                        grid[row][col - 1] = 2
-33                        q.append((row, col - 1))
-34                if col < cols - 1:
-35                    if grid[row][col + 1] == 1:
-36                        fresh -= 1
-37                        grid[row][col + 1] = 2
-38                        q.append((row, col + 1))
-39            output += 1
-40                    
-41        return output if fresh == 0 else -1
-42
-43
+13        
+14
+15        mins = 0
+16        while q:
+17            mins += 1
+18            length = len(q)
+19            for i in range(length):
+20                row, col = q.popleft()
+21                for dir in directions:
+22                    x, y = dir
+23                    newR = x + row
+24                    newC = y + col
+25                    if newR >= 0 and newR < ROWS and newC >= 0 and newC < COLS and grid[newR][newC] == 1:
+26                        q.append((newR, newC))
+27                        grid[newR][newC] = 2
+28
+29        for r in range(ROWS):
+30            for c in range(COLS):
+31                if grid[r][c] == 1:
+32                    return -1
+33        
+34        return mins - 1 if mins > 0 else 0
