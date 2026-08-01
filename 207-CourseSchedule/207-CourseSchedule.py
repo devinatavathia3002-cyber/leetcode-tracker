6e2a78c32a2824ca01dc207239a25c406130fbd8
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-08-01 15:01:37 -0700

    File updated at 8/1/2026, 3:01:37 PM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,30 +1,30 @@
-# Last updated: 6/13/2026, 4:49:33 PM
+# Last updated: 8/1/2026, 3:01:37 PM
 1class Solution:
 2    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-3        
-4        pre = defaultdict(list)
-5        visited = set()
-6        for pair in prerequisites:
-7            past, post = pair
-8            pre[past].append(post)
-9        
-10        def dfs(num):
-11            nonlocal visited
-12            if pre[num] == []:
-13                return True
-14            if num in visited:
-15                return False
-16            
-17            visited.add(num)
-18            for prereq in pre[num]:
-19                if not dfs(prereq):
-20                    return False
-21            
-22            visited.remove(num)
-23            pre[num] = []
-24            return True
-25            
-26        for num in range(numCourses):
-27            if not dfs(num):
-28                return False
-29        return True
+3        count = defaultdict(int)
+4        adj = defaultdict(list)
+5        total = 0
+6
+7        for crs in range(numCourses):
+8            count[crs] = 0
+9
+10        for prereq in prerequisites:
+11            crs, bf = prereq
+12            adj[crs].append(bf)
+13            count[bf] += 1
+14        
+15        q = deque()
+16        for key, val in count.items():
+17            if val == 0:
+18                q.append(key)
+19        
+20        while q:
+21            crs = q.popleft()
+22            total += 1
+23            for val in adj[crs]:
+24                count[val] -= 1
+25                if count[val] == 0:
+26                    q.append(val)
+27        
+28        return True if total == numCourses else False
+29
