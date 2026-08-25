29657b5845065b4efb17e43094808632ea3d5db0
devinatavathia3002-cyber <devinatavathia3002@gmail.com>
2026-08-24 23:19:03 -0700

    File updated at 8/24/2026, 11:19:02 PM

--- 1652-DefusetheBomb/1652-DefusetheBomb.py
+++ 1652-DefusetheBomb/1652-DefusetheBomb.py
@@ -1,4 +1,4 @@
-# Last updated: 8/24/2026, 10:37:49 PM
+# Last updated: 8/24/2026, 11:19:02 PM
 1class Solution:
 2    def decrypt(self, code: List[int], k: int) -> List[int]:
 3        n = len(code)
@@ -8,19 +8,22 @@
 7            return newC
 8        elif k < 0:
 9            num = abs(k)
-10            for i in range(len(code)):
-11                total = 0
-12                for j in range(1, num + 1):
-13                    index = i - j
-14                    total += code[index % n]
-15                newC[i] = total
-16        else:
-17            num = abs(k)
-18            for i in range(len(code)):
-19                total = 0
-20                for j in range(1, num + 1):
-21                    index = i + j
-22                    total += code[index % n]
+10            total = 0
+11            for j in range(1, num + 1):
+12                total += code[-j % n]
+13            for i in range(len(code)):
+14                newC[i] = total
+15                total += code[i]
+16                total -= code[(i - num) % n]
+17        else:
+18            num = abs(k)
+19            total = 0
+20            for j in range(1, num + 1):
+21                total += code[j % n]
+22            for i in range(len(code)):
 23                newC[i] = total
-24
-25        return newC
+24                if i < len(code) - 1:
+25                    total -= code[i + 1]
+26                    total += code[(i + 1 + k) % n]
+27
+28        return newC
